9f49793e5cd2782ddfc52de421e057cefc252cb2
drewroen <drewroen@gmail.com>
2024-12-24 12:30:59 -0600

    Add RoomService

--- websocket/src/main.py
+++ websocket/src/main.py
@@ -1,40 +1,27 @@
 #!/usr/bin/env python3
 
-import obj.chess as chess
-from obj.objects import Piece, Position
-import obj.room as gameRoom
+from obj.objects import Position
 
 from flask import Flask, request
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask_socketio import SocketIO, emit
+
+from svc.room import RoomService
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*", allow_eio3=True)
-
-
-player_rooms = {}
-rooms = {}
+room_service = RoomService()
 
 
 @socketio.on("connect")
 def test_connect():
-    if request.sid not in player_rooms:
-        for room_id in rooms:
-            room = rooms[room_id]
-            if room.open():
-                player_rooms[request.sid] = room
-                join_room(room_id)
-                room.join(request.sid)
-                break
-        else:
-            room = gameRoom.Room()
-            rooms[room.id] = room
-            player_rooms[request.sid] = room
-    else:
-        room = player_rooms[request.sid]
-
-    print(request.sid + " connected to room " + str(room.id))
-    emit("board", {"squares": room.board.get_squares()}, to=room.id)
+    room_id = room_service.add(request.sid)
+    print(request.sid + " connected to room " + str(room_id))
+    emit(
+        "board",
+        {"squares": room_service.get_room(room_id).board.get_squares()},
+        to=room_id,
+    )
 
 
 @socketio.on("disconnect")
@@ -47,7 +34,7 @@ def tileClicked(data):
     row, col = data
     notation = chr(col + ord("a")) + str(8 - row)
 
-    room = player_rooms[request.sid]
+    room = room_service.get_player_room(request.sid)
 
     moves = room.board.get_available_moves(notation)
     emit("tileClicked", [move.to_dict() for move in moves], to=room.id)
@@ -58,7 +45,7 @@ def movePiece(data):
     start = Position(data["from"][0], data["from"][1])
     end = Position(data["to"][0], data["to"][1])
 
-    room = player_rooms[request.sid]
+    room = room_service.get_player_room(request.sid)
 
     room.board.move(start, end)
     emit("board", {"squares": room.board.get_squares()}, to=room.id)
---
+++ websocket/src/svc/room.py
@@ -0,0 +1,32 @@
+from obj.room import Room
+from flask_socketio import join_room
+
+
+class RoomService:
+    def __init__(self):
+        self.rooms = {}
+        self.id_to_room_map = {}
+
+    def add(self, websocket_id: str):
+        for room in self.rooms.values():
+            if room.open():
+                room.join(websocket_id)
+                join_room(room.id)
+                self.id_to_room_map[websocket_id] = room
+                return room.id
+        new_room = Room()
+        new_room.join(websocket_id)
+        join_room(new_room.id)
+        self.rooms[new_room.id] = new_room
+        self.id_to_room_map[websocket_id] = new_room
+        return new_room.id
+
+    def get_player_room(self, player_id: int):
+        room = self.id_to_room_map.get(player_id)
+        print(self.id_to_room_map)
+        print(player_id)
+        print(room)
+        return room
+
+    def get_room(self, room_id: str):
+        return self.rooms.get(room_id)
