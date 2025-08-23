ae9ca3b5caf772d987a14573bcbc04496f11ee68
drewroen <drewroen@gmail.com>
2025-08-22 21:28:11 -0500

    Test

--- backend/app/routers/health.py
+++ backend/app/routers/health.py
@@ -5,7 +5,7 @@ router = APIRouter()
 
 @router.get("/")
 async def root():
-    return {"message": "Chess CG API is running!"}
+    return {"message": "Chess CG API is running. Use /health to check health."}
 
 
 @router.get("/health")
