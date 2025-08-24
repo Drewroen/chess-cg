0d138c2eaa8986a0d5eaedda7d1cfadb6f49b5c9
drewroen <drewroen@gmail.com>
2025-08-24 16:06:07 -0500

    Try hash router

--- frontend/chess-cg-ui/src/App.tsx
+++ frontend/chess-cg-ui/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { GameView } from "./components/GameView";
 import { AuthCallback } from "./components/AuthCallback";
 import { AuthSuccess } from "./components/AuthSuccess";
